6ee09a8d0b4e74c001b2d6a3aec9a9e0a5c4a549
DGalexander <dga.1984@gmail.com>
2016-02-04 21:00:13 +0000

    Added .R and .png Files

---
+++ Plot 1.R
@@ -0,0 +1,16 @@
+## Import Data
+data<- read.table("household_power_consumption.txt", sep=";", header=T, quote= "", strip.white=TRUE, stringsAsFactors = F, na.strings= "?")
+
+# Subset tData
+data<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
+
+## Plot 1
+png("plot1.png", width=480, height= 480)
+
+hist(data$Global_active_power, 
+     col= "red", 
+     xlab= "Global Active Power (kilowatts)", 
+     ylab= "Frequency", 
+     main= "Global Active Power")
+
+dev.off()
